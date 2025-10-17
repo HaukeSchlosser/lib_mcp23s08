9fc01efdf818a475d846c3cbff77bdca490f4719
hschloss <hauke.schlosser@pm.me>
2025-10-17 13:35:50 +0200

    lib_mcp23s08_h: 	- Minor change in comments 	- Add read only register bits

--- src/lib_mcp23s08.h
+++ src/lib_mcp23s08.h
@@ -8,10 +8,14 @@
 #define SPI_BPW     0x08
 #define SPI_DELAY   0x00
 #define SPI_WDELAY  0x00
-#define SPI_SPEED   0x186A0 // Supports up to 100000
+#define SPI_SPEED   0x186A0 // Supports up to 100000 Hz
 
 // MCP23S08 Device Type
 #define DEV_TYPE    0x40
+
+// Console Output Control
+#define NOT_SILENT  0x00
+#define SILENT      0x01
 
 // W/R Commands
 #define WRITE_CMD   0x00
@@ -40,8 +44,8 @@
 #define GPINTEN_DISABLE 0x00 // Disable GPIO pin for interrupt-on-change
 #define GPINTEN_ENABLE  0x01 // Enable GPIO pin for interrupt-on-change
 // MCP23S08 DEFVAL Bits
-#define DEFVAL_0        0x00 // Default comparison value
-#define DEFVAL_1        0x01 // Default comparison value
+#define DEFVAL_0        0x00 // Bit 0 comparison default = 0
+#define DEFVAL_1        0x01 // Bit 0 comparison default = 1
 // MCP23S08 INTCON Bits
 #define INTCON_PREV     0x00 // Value is compared against previous value
 #define INTCON_COMP     0x01 // Value is compared against DEFVAL bit
@@ -59,6 +63,12 @@
 // MCP23S08 GPPU Bits
 #define GPPU_DISABLE    0x00 // Pull-up disabled
 #define GPPU_ENABLE     0x01 // Pull-up enabled
+// MCP23S08 INTF Bits   -- READ-ONLY
+#define INTF_LOW        0x00 // Interrupt not pending
+#define INTF_HIGH       0x01 // Pin caused interrupt
+// MCP23S08 INTCAP Bits -- READ-ONLY
+#define INTCAP_LOW      0x00 // Logic Low
+#define INTCAP_HIGH     0x01 // Logic High
 // MCP23S08 GPIO Bits
 #define GPIO_LOW        0x00 // Logic Low
 #define GPIO_HIGH       0x01 // Logic High
@@ -71,6 +81,7 @@
  *
  * @param bus   SPI bus number (0 or 1).
  * @param cs    Chip select line (0 or 1).
+ * 
  * @return int  File descriptor for the opened SPI device, or -1 on failure.
  */
 int mcp23s08_init(uint8_t bus, uint8_t cs);
@@ -82,6 +93,7 @@ int mcp23s08_init(uint8_t bus, uint8_t cs);
  * @param addr      MCP23S08 device address (3-bit hardware address).
  * @param reg       Register address to write to.
  * @param data      Byte of data to write.
+ * 
  * @return int8_t   Returns 0 on success, or -1 on failure.
  */
 int8_t mcp23s08_write(int fd, uint8_t addr, uint8_t reg, uint8_t data);
@@ -92,9 +104,11 @@ int8_t mcp23s08_write(int fd, uint8_t addr, uint8_t reg, uint8_t data);
  * @param fd        File descriptor for the SPI device.
  * @param addr      MCP23S08 device address (3-bit hardware address).
  * @param reg       Register address to read from.
+ * @param silent    If 1, suppresses console output (0 or 1).
+ * 
  * @return int8_t   The received byte from the SPI response, or -1 on failure.
  */
-int16_t mcp23s08_read(int fd, uint8_t addr, uint8_t reg);
+int16_t mcp23s08_read(int fd, uint8_t addr, uint8_t reg, uint8_t silent);
 
 /**
  * @brief Writes a value to a specific pin of the MCP23S08 I/O expander.
@@ -127,6 +141,7 @@ int8_t mcp23s08_read_pin(int fd, uint8_t addr, uint8_t reg, uint8_t pin);
  * @param fd        File descriptor for the SPI device.
  * @param addr      MCP23S08 device address (3-bit hardware address).
  * @param pins      Bitmask specifying the new state for the selected pins (o = OUTPUT, 1 = INPUT).
+ * 
  * @return uint8_t  Returns 0 on success, or -1 on failure.
  */
 int8_t mcp23s08_set_dir(int fd, uint8_t addr, uint8_t pins);
@@ -136,6 +151,7 @@ int8_t mcp23s08_set_dir(int fd, uint8_t addr, uint8_t pins);
  *
  * @param cmd       The command bit (0 for write, 1 for read).
  * @param addr      The 3-bit hardware address of the MCP23S08 device.
+ * 
  * @return uint8_t  The computed control byte for SPI communication.
  */
 static uint8_t get_control_byte(uint8_t cmd, uint8_t addr);
